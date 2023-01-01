678472d95d75c9cfceaaca12a8a17b516db4649a
Geovanny Gil <esgeovannygil@gmail.com>
2023-01-01 16:14:15 -0600

    Config theme of storybook for IDC FORMS and favicon

---
+++ .storybook/YourTheme.js
@@ -0,0 +1,21 @@
+import { create } from '@storybook/theming'
+import { colors } from '../src/tokens'
+
+export default create({
+  base: 'light',
+  colorPrimary: colors.primary[500],
+
+  appBg: colors.neutral[100],
+  appBorderColor: colors.neutral[200],
+  appBorderRadius: 4,
+
+  // Toolbar default and active colors
+  barTextColor: colors.primary[400],
+  barSelectedColor: colors.primary[500],
+  barBg: colors.neutral[100],
+
+  brandTitle: 'IDC FORM DESIGN SYSTEM',
+  brandUrl: 'https://idcform-ds.geovannygil.com',
+  brandImage: '/images/brandImage.png',
+  brandTarget: '_self',
+});
