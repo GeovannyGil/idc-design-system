7e4256cde0ab9f9eea60d19f9c2dd6054bb33be8
Geovanny Gil <esgeovannygil@gmail.com>
2023-01-01 19:12:43 -0600

    Add plugin of status of components

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,7 +6,8 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    '@etchteam/storybook-addon-status'
   ],
   "framework": "@storybook/react",
   "staticDir": ['../assets'],
